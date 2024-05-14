58f0cb75c1c7c847d74aca25e06256905405a34c
jayalwayscodes <johnsonjardocs@gmail.com>
2024-05-14 06:49:55 +0100

    modified

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -12,22 +12,22 @@ case $1 in
             fi
         done
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo clean --manifest-path=./src/$program/Cargo.toml
+            cargo clean --manifest-path=../src/$program/Cargo.toml
         done
         rm -rf dist/program
         ;;
     "clean")
         rm -rf ./node_modules
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo clean --manifest-path=./src/$program/Cargo.toml
+            cargo clean --manifest-path=../src/$program/Cargo.toml
         done;;
     "build")
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
+            cargo build-bpf --manifest-path=../src/$program/Cargo.toml --bpf-out-dir=./dist/program
         done;;
     "deploy")
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
+            cargo build-bpf --manifest-path=../src/$program/Cargo.toml --bpf-out-dir=../dist/program
             solana program deploy dist/program/$program.so
         done;;
     "reset-and-build")
@@ -40,9 +40,9 @@ case $1 in
         done
         rm -rf dist/program
         for program in "${SOLANA_PROGRAMS[@]}"; do
-            cargo clean --manifest-path=./src/$program/Cargo.toml
-            cargo build-bpf --manifest-path=./src/$program/Cargo.toml --bpf-out-dir=./dist/program
-            solana program deploy dist/program/$program.so
+            cargo clean --manifest-path=../src/$program/Cargo.toml
+            cargo build-bpf --manifest-path=../src/$program/Cargo.toml --bpf-out-dir=../dist/program
+            solana program deploy ../dist/program/$program.so
         done
         npm install
         solana program show --programs
